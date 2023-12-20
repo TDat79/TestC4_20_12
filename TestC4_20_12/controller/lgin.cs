cd29fddc84f2c3855c4e69e52a5c09b65451bb93
Hoang Le Tom <hoangtom891@gmail.com>
2023-12-20 14:12:19 +0700

    Tom sua

---
+++ TestC4_20_12/controller/lgin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestC4_20_12.controller
+{
+    internal class lgin
+    {
+    }
+}
